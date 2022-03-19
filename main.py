7a4049a43ab3f1a1c688e26718786fc24e31e7a7
Antra Tripathi <antra_tripathi@yahoo.com>
2022-03-19 19:35:33 +0000

    adding route to read data

--- main.py
+++ main.py
@@ -1,34 +1,62 @@
+import os
 import pickle
 import pandas as pd
 from model_files.model import predict_data
+#from werkzeug import secure_filename
+from flask import Flask, request, jsonify, render_template, redirect, url_for
 
-from flask import Flask, request, jsonify
+path = os.getcwd()
+os.chdir(path)
+file_path=''
+
+def predict(df):   
+    
+    #data= request.get_json()
+    #df = pd.DataFrame.from_dict(data, orient='columns')
+    uuid_list = df['uuid']
+    #print(df)
+    model_path = r'C:\Users\antra\OneDrive - Queen Mary, University of London\Github\klarna_tech_assesment\model_files\lg_model3_pkl'    
+    with open(model_path, 'rb') as f_in:
+        model = pickle.load(f_in)
+        f_in.close()
+    predictions = predict_data(df, model)
+
+    result = {
+        'uuid': list(uuid_list),
+        'pd' : list(predictions)
+    }
+    return result
 
 ##creating a flask app and naming it "app"
 app = Flask('app')
 
 @app.route('/test')
 def test():
-    return 'Pinging Model Application!!'
+    return ('Pinging Model Application!! from {}'.format(path))
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    data= request.get_json()
-    df = pd.DataFrame.from_dict(data, orient='columns')
-    print(df)
+@app.route('/')
+def form():
+    return render_template('form.html')
 
-    path = r'C:\Users\antra\OneDrive - Queen Mary, University of London\Github\klarna_tech_assesment\model_files\lg_model3_pkl'
+@app.route('/', methods=['POST'])
+def get_data():
+    if request.method == 'POST':
+        file = request.files['file']
+
+        if file.filename != '':
+            file_path = path+'\\file.csv'
+            file.save(file_path)
+        
+        return(redirect(url_for('results')))
+
+@app.route('/results')
+def results():
+    file_path = path+'\\file.csv'
+    df = pd.read_csv(file_path)
+    result= predict(df)
+    res = pd.DataFrame.from_dict(result, orient='columns')
     
-    with open(path, 'rb') as f_in:
-        model = pickle.load(f_in)
-        f_in.close()
-
-    predictions = predict_data(df, model)
-
-    result = {
-        'test_prediction': list(predictions)
-    }
-    return jsonify(result)
+    return render_template('results.html', tables =[res.to_html(classes='data', header="true")])    
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port = 9696)
