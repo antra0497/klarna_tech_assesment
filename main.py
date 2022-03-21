80763552383332dfdd80895ff34dde454e142d28
Antra Tripathi <antra_tripathi@yahoo.com>
2022-03-21 00:36:32 +0000

    Added comments

--- main.py
+++ main.py
@@ -2,25 +2,30 @@ import os
 import pickle
 import pandas as pd
 from model_files.model import predict_data
-#from werkzeug import secure_filename
-from flask import Flask, request, jsonify, render_template, redirect, url_for
+from flask import Flask, request, render_template, send_file, make_response
 
 path = os.getcwd()
 os.chdir(path)
-file_path=''
 
 def predict(df):   
-    
-    #data= request.get_json()
-    #df = pd.DataFrame.from_dict(data, orient='columns')
-    uuid_list = df['uuid']
-    #print(df)
-    model_path = r'C:\Users\antra\OneDrive - Queen Mary, University of London\Github\klarna_tech_assesment\model_files\lg_model3_pkl'    
+    '''
+    Input: Dataframe
+    Output: Result dataframe, log file 
+    '''
+    uuid_list = df['uuid']  
+
+    #defining the model file path  
+    model_path = path + '\model_files\lg_model3_pkl' 
+
+    #reading the model file
     with open(model_path, 'rb') as f_in:
         model = pickle.load(f_in)
         f_in.close()
+
+    #calling predict_data from model.py file       
     predictions, log = predict_data(df, model)
 
+    #preparing result as json
     result = {
         'uuid': list(uuid_list),
         'pd' : list(predictions)
@@ -36,17 +41,23 @@ def test():
 
 @app.route('/')
 def form():
+    #rendering the form.html as first page to take the input
     return render_template('form.html')
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['POST', 'GET'])
 def get_data():
+
     if request.method == 'POST':
         file = request.files['file']   
+        #changing the file response into a dataframe
         df = pd.read_csv(file)
+        #calling the predict function defined above
         result, log= predict(df)
         res = pd.DataFrame.from_dict(result, orient='columns')
-    
-    return render_template('results.html', tables =res.to_dict(orient='records'), logs =log)    
+        res.to_csv(path+'/static/output.csv') 
+        #redenting the result.html with the result and the log 
+        return render_template('results.html', tables =res.to_dict(orient='records'), logs =log)
+ 
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port = 9696)
--- model_files/model.py
+++ model_files/model.py
@@ -7,9 +7,13 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 
 def drop_nan_col(data):
+  '''
+  This function takes a dataframe as input and drops the columns
+  which has more than 40% of the data as NaN. It returns the new dataframe.
+  '''
   row_count =data.shape[0] #total row count
   threshold = row_count*0.40 #defining threshold of 40% of total size
-  counter =0
+  counter =0 #counter flag
 
   for col in data:
     if col == 'default': #skipping the default column
@@ -18,82 +22,120 @@ def drop_nan_col(data):
       data= data.drop(columns=col, axis=1)
       counter +=1
   l ='{} columns were droped, they had more than 40% data missing, New shape of data is {}'.format(counter, data.shape)
-  return data, l
+  return data, l #returning the modified df and log
 
 def fill_nan(data):
+  '''
+  This function takes dataframe as an input and 
+  fill zero in place of NAN and returns the modified dataframe.
+  '''
   count =0
   for col in data.columns:
     if col == 'default': #skipping the default column
       pass
-    elif data[col].isnull().sum()!=0:
-      data[col] = data[col].fillna(0)
+    elif data[col].isnull().sum()!=0: #checking if column has any NAN values
+      data[col] = data[col].fillna(0) #fill those NAN with zero
       count +=1
   l='NaN values in {} columns filled with zero'.format(count)   
-  return data, l
+  return data, l #returning the modified df and log
 
 def convert_c2n(data):
+  '''
+  This function takes data frame as an input and converts 
+  all the categorical or boolean features into numerical features,
+  and returns the modified dataframe
+  Here we are using Ordinal Encoder to execute the task.
+  '''
   counter =0
-  encoder = OrdinalEncoder()
+  encoder = OrdinalEncoder() #initiallizing the encoder
   for col in data.columns:
-    if data[col].dtype == 'O' or data[col].dtype == 'bool':
+    if data[col].dtype == 'O' or data[col].dtype == 'bool': #checking for object or boolean data type
       data[col]= encoder.fit_transform(data[col].values.reshape(-1,1))
       counter +=1
   l ='{} columns converted from Categorical/Boolean to Numerical '.format(counter)
-  return data, l
+  return data, l #returning the modified df and log
 
 def standard_scaling(data, test_flag = False):
-
-    print(data.shape) 
-    x_std = StandardScaler()
-    # Standardizing the features, to bring all the featues on a scale between [0,1]
-    x= x_std.fit_transform(data)
+  '''
+  This function is used for normalising the data. 
+  We use Standard scaler to to fit tranform the data 
+  on the scale of [0,1] using z-score. 
+  '''
+  print(data.shape) 
+  x_std = StandardScaler()
+  # Standardizing the features, to bring all the featues on a scale between [0,1]
+  x= x_std.fit_transform(data)
     
-    x =pd.DataFrame(x)
-    return x
+  x =pd.DataFrame(x)
+  return x
 
 def dimensionality_reduction(data):
-
-    print(type(data))
-    pca = PCA(n_components=26) # with variance of 95%
-    principalComponents = pca.fit_transform(data)
-
-    print('Explained Variance: ', pca.explained_variance_)
-    print('\n')
-    print('Count of PCA components: ', pca.n_components_)
-    print('\n')
-    print('Visulating the variance vs no.of components')
-
-    v ='Explained Variance is {}'.format(pca.explained_variance_)
-    nc ='Count of PCA components: {}'.format(pca.n_components_)
-    return principalComponents, v, nc
+  '''
+  This function takes a data frame as input and apply PCA for dimentionality reduction
+  and returns the components which has high variance and will have more feature importance 
+  In this case we have initialized PCA for 95% variance.
+  '''
+  #print(type(data))
+  pca = PCA(n_components=26) # with variance of 95%
+  principalComponents = pca.fit_transform(data)
+  v ='Explained Variance is {}'.format(pca.explained_variance_)
+  nc ='Count of PCA components: {}'.format(pca.n_components_)
+  return principalComponents, v, nc #returning the modified df and log
 
 def data_prep(data):
-  log=['.',] # list to maintain log
-  log.append('Original shape of data: {}'.format(data.shape))
+  '''
+  Funtion: Data Cleaning Pipeline
+
+  Input: Raw Test Data
+  Output: Processed Test Data
+  Processing: Dropping columns, Changing column type, Filling Null values,
+              Sandard Scaling, Dimentionality reduction
+  '''
+
+  log=[] # list to maintain extra details as log
+  log.append('Original shape of Test data: {}'.format(data.shape))
 
   log.append('DATA PROCESSING')
+  #Step1 : Dropping columns with 40% values as NAN
   df, _ = drop_nan_col(data)
   log.append(_)
+
+  #Step 2: Changing categorical to numerical columns
   df_c, _ = convert_c2n(df)
   log.append(_)  
+
+  #Step 3: Filling null values with zero
   df_n, _ = fill_nan(df_c)
   log.append(_)  
+
+  #Step 4: Data Normalization
   df_norm = standard_scaling(df_n, test_flag=True)
+
+  #Step 5: Feature reduction
   df_reduced, v, nc= dimensionality_reduction(df_norm)
   log.append('DIMENTIONALITY REDUCTION')
   log.append(nc)
   log.append(v)
   
+  #returning processed test dataframe
   return df_reduced, log
 
 def predict_data(config, model):
+  '''
+  It takes the raw test data form main.py, 
+  and returns the predicted values with other details as log   
+  '''
+  #changing the received file intp pandas dataframe
+  df = pd.DataFrame(config)
+  
+  #Calling Data Cleaning Pipeline
+  df_processed, log = data_prep(df)
 
-    df = pd.DataFrame(config)
+  #Getting results
+  y_pred = model.predict(df_processed)
 
-    df_processed, log = data_prep(df)
-    y_pred = model.predict(df_processed)
-
-    return y_pred, log
+  #returning the results
+  return y_pred, log
 
 
   
